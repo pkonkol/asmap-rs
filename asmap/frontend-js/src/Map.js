4dc1b70a338f539b839f6b55d02532135883f6af
pkonkol <31186361+pkonkol@users.noreply.github.com>
2024-03-17 23:25:25 +0100

    plain leaflet map in js

---
+++ asmap/frontend-js/src/Map.js
@@ -0,0 +1,22 @@
+import React, { useRef } from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+const Map = () => {
+    const mapRef = useRef(null);
+    // geometric center of poland
+    const latitude = 52.11431;
+    const longitude = 19.423672;
+
+    return (
+        <MapContainer center={[latitude, longitude]} zoom={13} ref={mapRef} style={{ height: "90vh", width: "90vw" }}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+        </MapContainer>
+    );
+};
+
+export default Map;
